56c3dec7436e2d32720c4d2443769a42e6226391
Francisco Luthra <fluthra@f0r2s1.codam.nl>
2026-04-20 16:41:23 +0200

    push

--- ft_printf.h
+++ ft_printf.h
@@ -10,4 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF
+# define FT_PRINTF
 
+#include "ft_printf.h"
+#include <unistd.h>
+#include <stdarg.h>
+
+void	ft_putchar_fd(char c, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+
+
+
+
+
+
+#endif
